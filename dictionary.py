a4385d9b084ceb58c7301c1b2d0114b46ed7d80b
Rupanshu Kumar <rupanshukumar0.5050@gmail.com>
2026-03-12 21:42:01 +0530

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,18 @@
+students = {}
 
+no_of_students = int(input("Enter the Number of student: "))
+
+for i in range(no_of_students):
+  roll_no = int(input("Enter your Roll no: ")
+  name_ = input("Enter your Name: ")
+  age = int(input("Enter your Age: ")
+  course = input("Enter your Course: ")
+
+       students[roll_no] = {
+         "Name" : name_,
+         "Age" : age,
+         "Course" : course
+       }
+print("\nStudent Records:")
+for l in students:
+  print(l, "i", student[l])
