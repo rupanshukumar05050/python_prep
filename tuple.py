ff45f8465ba37dceb987ca2bc267717f0fcb98fa
Rupanshu Kumar <rupanshukumar0.5050@gmail.com>
2026-03-12 20:16:00 +0530

    tuple.py

--- tuple.py
+++ tuple.py
@@ -1,1 +1,10 @@
+name_ = input("Enter your Name: ")
+age_ = int(input("Enter your age: "))
+prof_ = input("Enter your profession: ")
+city_ = input("Enter your permanent address: ")
 
+tpl = (name_, age_, prof_, city_)
+
+print("Tuple Based Output")
+print("--"*50)
+print(tpl)
